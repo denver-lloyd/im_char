b878477266bd352978befa19fc620c395ef18b42
dlly <denver.lloyd@ams-osram.com>
2023-07-19 14:18:32 -0600

    updates to formatting

--- im_char/stats_engine/stats.cpp
+++ im_char/stats_engine/stats.cpp
@@ -10,9 +10,17 @@ using std::make_tuple;
 #include <cmath>
 #include "../utilities/utilities.cpp"
 
+
+/**
+ * Get the average offset of an image
+ *
+ * @param img 2D vector of pixel data
+ *
+ * @return avg offset of 2D array
+*/
 double avg_offset(vector<vector<float>>& img){
-    /** get the average offset of image */
-    float avg, summ;
+    float avg;
+    float summ;
     int rows, cols;
     tuple<int, int> rows_cols;
 
@@ -32,10 +40,18 @@ double avg_offset(vector<vector<float>>& img){
     return summ / (rows * cols);
 }
 
+/**
+ * Calculate spatial variance of an image
+ *
+ * @param img 2D vector of pixel data
+ *
+ * @return spatial variance of img
+*/
 double spatial_variance(vector<vector<float>>& img){
-    /** calculate spatial variance */
-    float spat_var, avg;
-    int rows, cols, val;
+    float spat_var;
+    float avg;
+    int rows, cols;
+    int val;
     tuple<int, int> rows_cols;
 
     // get dims
@@ -58,25 +74,31 @@ double spatial_variance(vector<vector<float>>& img){
     return spat_var /= (rows * cols);
 }
 
-/** calculate column averages */
+/**
+ * Calculate column averages of an image
+ *
+ * @param img 2D vector of pixel data
+ *
+ * @return column averages of image
+*/
 vector<float> col_avgs(vector<vector<float>>& img){
-    float col_var=0, summ=0, avg=0;
+    float col_var = 0;
+    float summ = 0;
+    float avg = 0;
     int rows, cols;
     tuple<int, int> rows_cols;
+    vector<float> col_avg(cols);
 
     // get dims
     rows_cols = get_dims(img);
     rows = get<0>(rows_cols);
     cols = get<1>(rows_cols);
 
-    // create array for storing column averages
-    vector<float> col_avg(cols);
-
     // get average of each column
     for(int col = 0; col < cols; col++)
     {
         summ = 0;
-        for(int row=0; row < rows; row++)
+        for(int row = 0; row < rows; row++)
         {
             // get the average column value
             summ += img[row][col];
@@ -89,9 +111,16 @@ vector<float> col_avgs(vector<vector<float>>& img){
     return col_avg;
 }
 
-/** calculate column average */
+/**
+ * Calculate column average
+ *
+ * @param img 2D vector of pixel data
+ *
+ * @return column average of image
+*/
 double col_avg(vector<vector<float>>& img){
-    float col_var=0, summ=0;
+    float col_var=0;
+    float summ=0;
     int rows, cols;
     tuple<int, int> rows_cols;
 
@@ -105,7 +134,7 @@ double col_avg(vector<vector<float>>& img){
     col_avgs_ = col_avgs(img);
 
     // calculate average of all columns
-    for(int col=0; col < cols; col++){
+    for(int col = 0; col < cols; col++){
         summ += col_avgs_[col];
     }
 
@@ -113,9 +142,20 @@ double col_avg(vector<vector<float>>& img){
 
 }
 
-/** calculate approximate column variance, this is not the exact solution */
-double col_var_cav(vector<vector<float>>& img, int L = 1){
-    float col_var=0, summ=0, avg=0;
+/**
+ * Calculate the appropriate column variance,
+ * this is not the exact solution
+ *
+ * @param img 2D vector of pixel data
+ * @param ttn_var total temporal noise of img
+ * @param L number of frames in frame stack
+ *
+ * @return approximate column variance
+*/
+double col_var_cav(vector<vector<float>>& img, float ttn_var = 0, int L = 1){
+    float col_var = 0;
+    float summ = 0;
+    float avg = 0;
     int rows, cols;
     tuple<int, int> rows_cols;
 
@@ -132,19 +172,134 @@ double col_var_cav(vector<vector<float>>& img, int L = 1){
     avg = col_avg(img);
 
     // calculate column variance
-    for(int col=0; col < cols; col++){
+    for(int col = 0; col < cols; col++){
         col_var += pow((col_avgs_[col] - avg), 2);
     }
+    col_var /= cols;
 
-    return col_var / cols;
+    // remove residual temporal noise
+    col_var -= ttn_var / (L * rows);
+
+    return col_var;
+}
+
+/**
+ * Calculate row averages
+ *
+ * @param img 2D vector of pixel data
+ *
+ * @return array of row averages
+*/
+vector<float> row_avgs(vector<vector<float>>& img){
+    float row_var = 0;
+    float summ = 0;
+    float avg = 0;
+    int rows, cols;
+    tuple<int, int> rows_cols;
+
+    // get dims
+    rows_cols = get_dims(img);
+    rows = get<0>(rows_cols);
+    cols = get<1>(rows_cols);
+
+    // create array for storing row averages
+    vector<float> row_avg(cols);
+
+    // get average of each row
+    for(int row = 0; row < rows; row++)
+    {
+        summ = 0;
+        for(int col=0; col < cols; col++)
+        {
+            // get the average row
+            summ += img[row][col];
+        }
+
+        // add average row value
+        row_avg[row] = summ / cols;
+    }
+
+    return row_avg;
+}
+
+/**
+ * Calculate row average
+ *
+ * @param img 2D vector of pixel data
+ *
+ * @return row average
+*/
+double row_avg(vector<vector<float>>& img){
+    float row_var = 0;
+    float summ = 0;
+    int rows, cols;
+    tuple<int, int> rows_cols;
+
+    // get dims
+    rows_cols = get_dims(img);
+    rows = get<0>(rows_cols);
+    cols = get<1>(rows_cols);
+
+    // create array for storing column averages
+    vector<float> row_avgs_(cols);
+    row_avgs_ = row_avgs(img);
+
+    // calculate average of all rows
+    for(int row = 0; row < rows; row++){
+        summ += row_avgs_[row];
+    }
+
+    return summ / cols;
+
+}
+
+/**
+ * Calculate the appropriate row variance,
+ * this is not the exact solution
+ *
+ * @param img 2D vector of pixel data
+ * @param ttn_var total temporal noise of img
+ * @param L number of frames in frame stack
+ *
+ * @return approximate row variance
+*/
+double row_var_cav(vector<vector<float>>& img, float ttn_var = 0, int L = 1){
+    float row_var=0, summ=0, avg=0;
+    int rows, cols;
+    tuple<int, int> rows_cols;
+
+    // get dims
+    rows_cols = get_dims(img);
+    rows = get<0>(rows_cols);
+    cols = get<1>(rows_cols);
+
+    // get row averages
+    vector<float> row_avgs_(cols);
+    row_avgs_ = row_avgs(img);
+
+    // get average of all rows
+    avg = row_avg(img);
+
+    // calculate row variance
+    for(int row=0; row < rows; row++){
+        row_var += pow((row_avgs_[row] - avg), 2);
+    }
+    row_var /= rows;
+
+    // remove residual temporal noise
+    row_var -= ttn_var / (L * cols);
+
+    return row_var / rows;
 }
 
 int main(){
+    //vector<vector<float>> img = {{100, 100}, {0, 0}};
     vector<vector<float>> img = {{0, 100}, {0, 100}};
     //input(img);
     cout << "average offset:" << avg_offset(img) << endl;
     cout << "spatial variance:" << spatial_variance(img) << endl;
     cout << "column average:" << col_avg(img) << endl;
     cout << "col spatial variance:" << col_var_cav(img) << endl;
+    cout << "row spatial variance:" << row_var_cav(img) << endl;
     }
 
